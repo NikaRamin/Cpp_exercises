68f18a649f39b834a45099bcc8f1f117f3711784
Nika Ramin <128173488+NikaRamin@users.noreply.github.com>
2023-09-26 22:18:01 +0330

    Update templates.cpp

--- templates.cpp
+++ templates.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+
+template<typename T>
+void myswap(T& a,T& b){
+    T temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+int main(){
+    int a = 97 , b = 90;
+    cout << "Original:"<<endl<<"a: "<<a<<" b: "<<b<<endl;
+    myswap<int>(a,b);
+    cout << "Swapped:"<<endl<<"a: "<<a<<" b: "<<b<<endl;
+    return 0;
+}
